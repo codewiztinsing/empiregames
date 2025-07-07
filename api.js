426aa9f7585850acd66bd5057758ecb4b02eca63
codewiztinsing <tinsingjobs2k@gmail.com>
2025-07-07 12:25:04 +0300

    new files

---
+++ api.js
@@ -0,0 +1,19 @@
+const submitWinner = async (game) => {
+    try {
+     
+      console.log("game = ",game)
+  
+  
+  
+  
+    } catch (error) {
+      console.error('Error submitting winner:', error);
+     
+    }
+  };
+  
+  
+  module.exports = {
+    submitWinner
+  };
+  
