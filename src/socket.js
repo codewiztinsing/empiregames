01973eafba70fca477b56ac9ec17df6843cc1acf
codewiztinsing <tinsingjobs2k@gmail.com>
2025-07-09 20:51:26 +0300

    working

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,6 @@
 import { io } from 'socket.io-client';
 
-// const URL = process.env.SERVER_URL; // Change to your backend URL
-const URL = "http://localhost:5000";
-console.log("URL", URL);
+const URL = process.env.SERVER_URL; // Change to your backend URL
 const socket = io(URL, {
   autoConnect: false, // we'll manually connect to control timing
   transports: ['websocket'],
