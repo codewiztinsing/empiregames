eeb94580f1885bdc6d2868e9969805f144b34e4a
codewiztinsing <tinsingjobs2k@gmail.com>
2025-07-20 20:47:53 +0300

    working

--- bingo/server.js
+++ bingo/server.js
@@ -167,7 +167,7 @@ async function startGame(game) {
     io.to(game.roomId).emit("gameState", {
       gameId: game.id,
       roomId: game.roomId,
-      total_players: game.selectedNumbers.length,
+      // total_players: game.selectedNumbers.length,
       pickedNumbers: game.selectedNumbers,
       game_status: game.status,
       count_down: game.countDown,
