fae0d9b7ce57e886a960d0a439ab373e3da6ca5c
codewiztinsing <tinsingjobs2k@gmail.com>
2025-07-22 22:01:06 +0300

    working

--- bingo/src/helpers/bingo.js
+++ bingo/src/helpers/bingo.js
@@ -64,62 +64,111 @@ function turnCalledNumbersToCard(calledNumbers){
     return card;
 }
 
-function checkBingo(playerCards,calledNumbers){
-    let isBingo = false;
+// function checkBingo(playerCards,calledNumbers){
+//     let isBingo = false;
 
-    for(let i = 0; i < playerCards.length; i++){
-        const playerCard = playerCards[i];
-        const markedCard = markPlayerCard(playerCard,calledNumbers);
-        // Check rows for bingo
-    for (let row = 0; row < 5; row++) {
-        if (markedCard[row].every(cell => cell.marked)) {
-            isBingo = true;
-            return isBingo;
-        }
-    }
+//     for(let i = 0; i < playerCards.length; i++){
+//         const playerCard = playerCards[i];
+//         const markedCard = markPlayerCard(playerCard,calledNumbers);
+//         // Check rows for bingo
+//             for (let row = 0; row < 5; row++) {
+//                 if (markedCard[row].every(cell => cell.marked)) {
+//                     isBingo = true;
+//                     return isBingo;
+//                 }
+//             }
 
-    // Check columns for bingo
-    for (let col = 0; col < 5; col++) {
-        if (markedCard.every(row => row[col].marked)) {
-            isBingo = true;
-            return isBingo;
-        }
-    }
+//         // Check columns for bingo
+//         for (let col = 0; col < 5; col++) {
+//             if (markedCard.every(row => row[col].marked)) {
+//                 isBingo = true;
+//                 return isBingo;
+//             }
+//         }
 
-    // Check diagonal from top-left to bottom-right
-    if (markedCard[0][0].marked && 
-        markedCard[1][1].marked && 
-        markedCard[2][2].marked && 
-        markedCard[3][3].marked && 
-        markedCard[4][4].marked) {
-        isBingo = true;
-        return isBingo;
-    }
+//     // Check diagonal from top-left to bottom-right
+//     if (markedCard[0][0].marked && 
+//         markedCard[1][1].marked && 
+//         markedCard[2][2].marked && 
+//         markedCard[3][3].marked && 
+//         markedCard[4][4].marked) {
+//         isBingo = true;
+//         return isBingo;
+//     }
 
-    // Check diagonal from top-right to bottom-left
-    if (markedCard[0][4].marked && 
-        markedCard[1][3].marked && 
-        markedCard[2][2].marked && 
-        markedCard[3][1].marked && 
-        markedCard[4][0].marked) {
-        isBingo = true;
-        return isBingo;
-    }
+//     // Check diagonal from top-right to bottom-left
+//     if (markedCard[0][4].marked && 
+//         markedCard[1][3].marked && 
+//         markedCard[2][2].marked && 
+//         markedCard[3][1].marked && 
+//         markedCard[4][0].marked) {
+//         isBingo = true;
+//         return isBingo;
+//     }
 
-    // Check four corners
-    if (markedCard[0][0].marked && 
-        markedCard[0][4].marked &&
-        markedCard[4][0].marked && 
-        markedCard[4][4].marked) {
-        isBingo = true;
-        return isBingo;
-    }
+//     // Check four corners
+//     if (markedCard[0][0].marked && 
+//         markedCard[0][4].marked &&
+//         markedCard[4][0].marked && 
+//         markedCard[4][4].marked) {
+//         isBingo = true;
+//         return isBingo;
+//     }
    
-    return isBingo;
-    }
-    return isBingo;
-}
+//     return isBingo;
+//     }
+//     return isBingo;
+// }
     
+
+function checkBingo(playerCards, calledNumbers) {
+    for (let i = 0; i < playerCards.length; i++) {
+        const playerCard = playerCards[i];
+        const markedCard = markPlayerCard(playerCard, calledNumbers);
+        
+        // Check rows for bingo
+        for (let row = 0; row < 5; row++) {
+            if (markedCard[row].every(cell => cell.marked)) {
+                return true;
+            }
+        }
+
+        // Check columns for bingo
+        for (let col = 0; col < 5; col++) {
+            if (markedCard.every(row => row[col].marked)) {
+                return true;
+            }
+        }
+
+        // Check diagonal from top-left to bottom-right
+        if (markedCard[0][0].marked && 
+            markedCard[1][1].marked && 
+            markedCard[2][2].marked && 
+            markedCard[3][3].marked && 
+            markedCard[4][4].marked) {
+            return true;
+        }
+
+        // Check diagonal from top-right to bottom-left
+        if (markedCard[0][4].marked && 
+            markedCard[1][3].marked && 
+            markedCard[2][2].marked && 
+            markedCard[3][1].marked && 
+            markedCard[4][0].marked) {
+            return true;
+        }
+
+        // Check four corners
+        if (markedCard[0][0].marked && 
+            markedCard[0][4].marked &&
+            markedCard[4][0].marked && 
+            markedCard[4][4].marked) {
+            return true;
+        }
+    }
+    
+    return false;
+}
    
 
 module.exports = { checkBingo,markPlayerCard,turnCalledNumbersToCard,turnMarkedCellsToCard };
