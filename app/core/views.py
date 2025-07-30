876e8f190b6fd9307f82841afcd294c028297cfc
codewiztinsing <tinsingjobs2k@gmail.com>
2025-07-30 12:01:10 +0300

    working

--- app/core/views.py
+++ app/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from users.models import User
 from game.models import Game
-from wallet.models import Wallet,Transaction
+from wallet.models import Wallet,Transaction,ChapaSession
 from datetime import datetime
 
 def custom_404(request, exception):
@@ -25,9 +25,9 @@ def dashboard_view(request):
     today_new_players = User.objects.filter(date_joined__date=datetime.now().date()).count()
   
     available_balance = sum([wallet.balance for wallet in wallets])
-
-    
-
+    total_success_chapa_amount = sum([chapa.amount for chapa in ChapaSession.objects.filter(status="success")])
+    today_withdrawals_amount = sum([transaction.amount for transaction in Transaction.objects.filter(created_at__date=datetime.now().date(),type="WITHDRAW")])
+    total_withdrawals_amount = sum([transaction.amount for transaction in Transaction.objects.filter(type="WITHDRAW")])
 
     context = {
         "total_players": total_players,
@@ -36,7 +36,10 @@ def dashboard_view(request):
         "today_games": today_games,
         "today_deposits": today_deposits,
         "today_withdrawals": today_withdrawals,
-        "today_new_players": today_new_players
+        "today_new_players": today_new_players,
+        "total_success_chapa_amount": total_success_chapa_amount,
+        "today_withdrawals_amount": today_withdrawals_amount,
+        "total_withdrawals_amount": total_withdrawals_amount,
     }
 
    
