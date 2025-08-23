a0262e150bb002a3443088a2913117abf94b1679
Tinsae alako <tinsingjobs2k@gmail.com>
2025-08-23 11:53:10 -0700

    working

--- bingo/src/socket.js
+++ bingo/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-const URL = "http://localhost:5000";
-// const URL = process.env.REACT_APP_SOCKET_URL;
+// const URL = "http://localhost:5000";
+const URL = "https://server.akerbingo.com";
 const socket = io(URL, {
   autoConnect: false, // we'll manually connect to control timing
   transports: ['websocket'],
