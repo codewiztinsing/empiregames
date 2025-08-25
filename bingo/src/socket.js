b95761abeae5754a37f2e524a7f23b67779a0d82
Tinsae alako <tinsingjobs2k@gmail.com>
2025-08-25 07:20:35 -0700

    wroig

--- bingo/src/socket.js
+++ bingo/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.REACT_APP_SERVER_URL
+const URL = process.env.REACT_APP_SOCKET_URL
 console.log("URL - ",URL)
 
 const socket = io(URL, {
