1b444788db6964fcdf8683a833d2259252bfdf4b
Tinsae alako <tinsingjobs2k@gmail.com>
2025-08-25 07:15:35 -0700

    working

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Navigate to the bingo directory
+cd bingo
+
+# Install dependencies
+npm install
+
+# Build the React application
+npm run build
+
+# Navigate to the app directory
+cd ../app
+
+# make sure templates/build directory exists
+mkdir -p templates/build
+
+
+
+# Remove all files from /templates/build directory
+rm -rf templates/build/*
+
+# Copy the new build files to /templates/build
+cp -r ../bingo/build/* templates/build/
+
+echo "Build completed"
