1890611c7ee71a91bf32feb50d8392f4f72b1048
Tinsae alako <tinsingjobs2k@gmail.com>
2025-08-25 12:08:17 -0700

    working

--- app/webbot.py
+++ app/webbot.py
@@ -172,8 +172,8 @@ async def get_withdraw_amount(update: Update, context: ContextTypes.DEFAULT_TYPE
         balance = float(wallet_response.get('balance', 0))
         logger.info(f"balance {balance}")
       
-        if int(balance) < 100:
-            await update.message.reply_text(f"You must leave at least 100 ETB in your wallet. Please enter a smaller amount.")
+        if int(balance) < 100000:
+            await update.message.reply_text(f"You must leave at least 100,000 ETB in your wallet. Please enter a smaller amount.")
             return WITHDRAW_AMOUNT_CONFIRM
 
 
