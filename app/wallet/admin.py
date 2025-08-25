cdbf9f1fd13a0875c86f19dd4bb74097178a4875
Tinsae alako <tinsingjobs2k@gmail.com>
2025-08-25 12:11:08 -0700

    working

--- app/wallet/admin.py
+++ app/wallet/admin.py
@@ -10,9 +10,12 @@ class WalletAdmin(admin.ModelAdmin):
 admin.site.register(Wallet, WalletAdmin)
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ( 'amount', 'type', 'status', 'reference')
+    list_display = ( 'amount', 'type', 'status', 'reference','user_username','user_telegram_id','created_at')
 
     list_filter = ('type', 'status')
+    search_fields = ('user__username', 'user__telegram_id','reference')
+    raw_id_fields = ('user',)
+    ordering = ('-created_at',)
     list_per_page = 10
 
 
