bc42d7e26e8d204fc19779ecca07e94e2a49859e
Tinsae alako <tinsingjobs2k@gmail.com>
2025-08-26 07:33:59 -0700

    working

--- app/users/admin.py
+++ app/users/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'phone', 'telegram_id', 'referral_code')
-    search_fields = ('username', 'email', 'phone', 'telegram_id')
+    list_display = ('id','username', 'phone', 'telegram_id')
+    search_fields = ('username', 'phone', 'telegram_id')
     list_filter = ('is_active', 'is_staff')
     list_per_page = 10
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'phone', 'telegram_id', 'referral_code')
-    search_fields = ('username', 'email', 'phone', 'telegram_id')
+    list_display = ("id",'username', 'phone', 'telegram_id')
+    search_fields = ('username', 'phone', 'telegram_id')
     list_filter = ('is_active', 'is_staff')
     list_per_page = 10
 
