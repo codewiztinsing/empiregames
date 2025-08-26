ebaa32a2411ee3070eb96773b04771ae7c68d1d3
Tinsae alako <tinsingjobs2k@gmail.com>
2025-08-26 09:04:24 -0700

    working

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path
+from .views import dashboard
 from .main import api
 
 
 def game_view(request):
-   
     return render(request, 'build/index.html')
+
+
+
+
 
 urlpatterns = [
     path('',game_view, name='game_view'),
+    path('dashboard/',dashboard,name = "dashboard"),
     path('admin/', admin.site.urls),
     path('api/v1/', api.urls),
     path('', game_view, name='game_view')
--- app/core/views.py
+++ app/core/views.py
@@ -1,4 +1,40 @@
 from django.shortcuts import render
+from users.models import User
+from wallet.models import Wallet, Transaction
+from game.models import Game
+from django.db.models import Sum
+
+
+
+
+
+def dashboard(request):
+    user = User.objects.all()
+    wallet = {
+        "total_balance": Wallet.objects.aggregate(Sum('balance'))['balance__sum'],
+        "total_withdrawals": Transaction.objects.filter(type='WITHDRAW').aggregate(Sum('amount'))['amount__sum'],
+        "total_deposits": Transaction.objects.filter(type='DEPOSIT').aggregate(Sum('amount'))['amount__sum'],
+    }
+    print(wallet)
+    game = Game.objects.all()
+    context = {
+        'user': user,
+        'wallet': wallet,   
+        'page_title': 'Dashboard Overview',
+        'games_played': game.count(),
+        'games_won': game.filter(ended=True).count(),
+        'win_rate': (game.filter(ended=True).count() / game.count()) * 100,
+        'current_streak': 0,
+        'best_streak': 0,
+        'recent_activities': [],
+        'statistics': {
+            'total_games': game.count(),
+            'wins': game.filter(ended=True).count(),
+            'losses': game.filter(ended=False).count(),
+            'draws': game.filter(ended=None).count(),
+        }
+    }
+    return render(request, 'dashboard/index.html',context)
 
 def custom_404(request, exception):
     print("404 error")
