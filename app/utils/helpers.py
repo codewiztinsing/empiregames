5cc7f0df3e0bb0f513237ae8aa9380d12198d752
Tinsae alako <tinsingjobs2k@gmail.com>
2025-08-27 00:00:28 -0700

    working

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -5,10 +5,8 @@ BACK_URL = config("BACK_URL")
 
 def daily_withdraw_limit(user_id):
     response = requests.get(f"{BACK_URL}/api/v1/wallet/users/{user_id}/daily-withdraw-limit")
-    if response.status_code == 200:
-        return response.json().get("daily_withdraw_limit")
-    else:
-        return 0
+    return response.json().get("daily_withdraw_limit",0)
+    
 
 def numnber_of_game_played(user_id):
     response = requests.get(f"{BACK_URL}/api/v1/users/{user_id}/number-of-game-played")
