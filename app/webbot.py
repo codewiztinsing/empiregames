9a32a0ac3004cd09d8de02be317f2164bf22c72d
Tinsae alako <tinsingjobs2k@gmail.com>
2025-08-30 08:35:10 -0700

    working

--- app/webbot.py
+++ app/webbot.py
@@ -77,7 +77,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         [InlineKeyboardButton("💰 Check Balance", callback_data='check_balance'),
          InlineKeyboardButton("💳 Deposit", callback_data='deposit')],
         [InlineKeyboardButton("📞 Contact Support", callback_data='contact_support'),
-         InlineKeyboardButton("📚 Instruction", callback_data='instructions')]
+         InlineKeyboardButton("📚 Instruction", callback_data='instructions')],
         [InlineKeyboardButton("🔗 Join Group", url='https://t.me/wowbingos')]
     ]
     
