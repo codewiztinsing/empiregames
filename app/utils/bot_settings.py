6d47fcbefc0c69815a1aae16c006cdbf1055022e
Tinsae alako <tinsingjobs2k@gmail.com>
2025-09-08 08:31:29 -0700

    working

--- app/utils/bot_settings.py
+++ app/utils/bot_settings.py
@@ -15,8 +15,7 @@ def initialize_manual_session(amount, session_id, phone_number,message):
     BACK_URL = config('BACK_URL')
     MANUAL_BASE_URL = config('MANUAL_BASE_URL') + "receipts/verify/"
     session_url = BACK_URL + "/api/v1/wallet/manual/session/"
-    # callbackurl = BACK_URL + "/api/v1/wallet/manual/callback/success/"
-    callbackurl = "https://webhook.site/c44a944a-1391-4b1b-9cd1-46238d5cb3f3"
+    callbackurl = BACK_URL + "/api/v1/wallet/manual/callback/success/"
     errorUrl = BACK_URL + "/api/v1/wallet/manual/callback/error/"
     data = {
         "amount":amount,
@@ -27,11 +26,9 @@ def initialize_manual_session(amount, session_id, phone_number,message):
     if response.status_code == 200:
         data = {"message": message,"callbackurl":callbackurl,"errorUrl":errorUrl,"paymentMethod":"telebirr"}
         response = requests.post(f"{MANUAL_BASE_URL}", json=data)
-        print("manual response = ",response.status_code)
         if response.status_code == 200:
             try:
                 json_data = response.json()
-                print("response = ", json_data)
                 return json_data
             except Exception:
                 print("response (non-JSON) = ", response.text)
