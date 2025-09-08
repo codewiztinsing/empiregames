5dc66381f707c765531f78fac2bd2d4440bbf5c4
Tinsae alako <tinsingjobs2k@gmail.com>
2025-09-08 14:48:59 -0700

    working

--- app/webbot.py
+++ app/webbot.py
@@ -643,7 +643,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
         elif query.data == 'manual':
             keyboard = [
-                [InlineKeyboardButton("Telebirr", callback_data='manual_telebirr')]
+                [InlineKeyboardButton("Telebirr", callback_data='manual_telebirrcc')]
             ]
             reply_markup = InlineKeyboardMarkup(keyboard)
             await query.message.reply_text(text="Please select a payment method:", reply_markup=reply_markup)
