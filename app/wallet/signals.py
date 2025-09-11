0b0389f28ae3d9a896b3749c944be7d12fc66c29
Tinsae alako <tinsingjobs2k@gmail.com>
2025-09-11 03:27:12 -0700

    working

--- app/wallet/signals.py
+++ app/wallet/signals.py
@@ -9,24 +9,24 @@ from django.core.exceptions import ObjectDoesNotExist, ValidationError
 
 logger = logging.getLogger(__name__)
 
-@receiver(post_save, sender=Transaction)
-def update_wallet_on_deposit(sender, instance, created, **kwargs):
-    """
-    Signal handler for updating wallet balance when transaction type is DEPOSIT and status is success.
-    """
-    print(f"Transaction: {instance.type}, {instance.status}")
-    if instance.type == "DEPOSIT" and instance.status == "success":
-        try:
-            with transaction.atomic():
-                wallet, created = Wallet.objects.get_or_create(user=instance.user)
-                wallet.balance += float(instance.amount)
-                wallet.save()
-                logger.info(f"Wallet balance updated for user {instance.user.username}. New balance: {wallet.balance}")
-                return wallet
-        except Exception as e:
-            logger.error(f"Error updating wallet for user {instance.user.username}: {str(e)}")
-            return None
-    return None
+# @receiver(post_save, sender=Transaction)
+# def update_wallet_on_deposit(sender, instance, created, **kwargs):
+#     """
+#     Signal handler for updating wallet balance when transaction type is DEPOSIT and status is success.
+#     """
+#     print(f"Transaction: {instance.type}, {instance.status}")
+#     if instance.type == "DEPOSIT" and instance.status == "success":
+#         try:
+#             with transaction.atomic():
+#                 wallet, created = Wallet.objects.get_or_create(user=instance.user)
+#                 wallet.balance += float(instance.amount)
+#                 wallet.save()
+#                 logger.info(f"Wallet balance updated for user {instance.user.username}. New balance: {wallet.balance}")
+#                 return wallet
+#         except Exception as e:
+#             logger.error(f"Error updating wallet for user {instance.user.username}: {str(e)}")
+#             return None
+#     return None
    
 
 @receiver(post_save, sender=Transaction)
