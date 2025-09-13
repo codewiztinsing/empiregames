1588a2c52963ca22ebaccad909379e49412203ea
Tinsae alako <tinsingjobs2k@gmail.com>
2025-09-13 07:21:05 -0700

    working

--- api-client/client.py
+++ api-client/client.py
@@ -1,18 +1,37 @@
 data = {
-
-  "amount": 51.75,
-            "currency": "ETB",
-            "tx_ref": "1234567890",
-            "phone_number": "251912345678",
-            "first_name":"alako",
-            "last_name":"abiyo",
-            "email":"alako@gmail.com"
+  "session_id": "9a098d29-27aa-4216-9177-2a774d6dcde10",
+  "status": "success",
+  "message": "Transaction verified successfully",
+  "data": {
+    "payer_name": "ESHETU FEYESSA ABERA",
+    "payer_telebirr_no": "2519****9495",
+    "payer_account_type": "Individual Customer",
+    "credited_party": "ESHETU FEYISSA ABERA",
+    "credited_account": "2519****4252",
+    "transaction_status": "የከፋይ ስም/Payer Name",
+    "invoice_no": "የክፍያ ቀን/Payment date",
+    "payment_date": "የክፍያ ዝርዝር/ Invoice details",
+    "settled_amount": "የክፍያ ዝርዝር/ Invoice details",
+    "service_fee": "0.87 Birr",
+    "total_paid": "የክፍያ ዝርዝር/ Invoice details",
+    "total_in_words": "fifty-one birr and zero cent",
+    "payment_mode": "telebirr",
+    "payment_reason": "Send Money to Registered Customer",
+    "payment_channel": "API/App",
+    "customer_note": "",
+    "amount": 51,
+    "ref_number": "CFJ6167JLC"
+  }
 }
-
 import requests
 
-url = "http://127.0.0.1:8000/api/v1/wallet/chapa/create-session"
+url = "http://127.0.0.1:8000/api/v1/wallet/manual/callback/success/"
+headers = {
+  "Content-Type": "application/json",
+  "Accept": "application/json"
+}
 
-response = requests.post(url, json=data)
+response = requests.post(url, json=data, headers=headers)
 
-print(response.status_code)
+print(f"Status Code: {response.status_code}")
+print(f"Response: {response.text}")
