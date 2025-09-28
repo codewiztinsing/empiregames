c68610c2c5b2e14cea0cae9c70365e515c3261e3
Tinsae alako <tinsingjobs2k@gmail.com>
2025-09-28 06:32:30 -0700

    bingo content

--- bingo/server.js
+++ bingo/server.js
@@ -544,7 +544,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-const PORT = 4000
+const PORT = process.env.PORT
 const IP = ip.address();
 server.listen(PORT, () => console.log(`Server running on port ${PORT} and IP ${IP}`));
 
