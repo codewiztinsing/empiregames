4b82b5a5a3b66a49d7ea32635311d5bbbd0d5695
tinsae <tinsingjobs2k@gmail.com>
2025-10-26 08:36:05 +0300

    working

--- app/bot/support.py
+++ app/bot/support.py
@@ -11,8 +11,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 import django
 django.setup()
 
-from users.models import User
-from wallet.models import Transaction, WithdrawalRequest
+from users.models import User, WithdrawalRequest
+from wallet.models import Transaction
 from asgiref.sync import sync_to_async
 
 # Set up logging
